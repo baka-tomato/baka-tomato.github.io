1b59601d07f916c702871fca29f80dbb5c4847ca
baka-tomato <kaizen.me2024@gmail.com>
2024-05-25 23:07:21 +0545

    updated start.js with

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 console.log('hello world.');
 console.log('The new feature has been added.');
 console.log('for practising merge');
-
+console.log('ahh so this is how checkout -b branch name works');
